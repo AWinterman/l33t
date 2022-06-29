fe72d762b61cedf8e901b8ac236a4b64dbe41a2c
Andrew Winterman <andywinterman@gmail.com>
2022-06-29 23:03:59 +0000

    a little more devspace fiddling
    Also simplify the quicksort doc

--- quicksort.go
+++ quicksort.go
@@ -2,40 +2,7 @@ package l33t
 
 import "golang.org/x/exp/constraints"
 
-/*
-// Sorts a (portion of an) array, divides it into partitions, then sorts those
-algorithm quicksort(A, lo, hi) is
-  // Ensure indices are in correct order
-  if lo >= hi || lo < 0 then
-    return
-
-  // Partition array and get the pivot index
-  p := partition(A, lo, hi)
-
-  // Sort the two partitions
-  quicksort(A, lo, p - 1) // Left side of pivot
-  quicksort(A, p + 1, hi) // Right side of pivot
-
-// Divides array into two partitions
-algorithm partition(A, lo, hi) is
-  pivot := A[hi] // Choose the last element as the pivot
-
-  // Temporary pivot index
-  i := lo - 1
-
-  for j := lo to hi - 1 do
-    // If the current element is less than or equal to the pivot
-    if A[j] <= pivot then
-      // Move the temporary pivot index forward
-      i := i + 1
-
-      // Swap the current element with the element at the temporary pivot index
-      swap A[i] with A[j]
-  // Move the pivot element to the correct pivot position (between the smaller and larger elements)
-  i := i + 1
-  swap A[i] with A[hi]
-  return i // the pivot index
-*/
+// Quicksort is a quicksort implementation that sorts Ordered values.
 func Quicksort[T constraints.Ordered](A []T) []T {
 	return quicksort(A, 0, len(A)-1)
 }
